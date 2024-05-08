2c25fb79d9301eddb74bbcf5f5bb8f8f8251e13e
warlord-2227 <setu.iitb@gmail.com>
2024-05-08 10:27:52 +0530

    hello_pubsub added

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import base64
+import json
+import numpy as np
+import pandas as pd
+import pickle
+import joblib
+import sklearn
+from google.cloud import storage, pubsub_v1
+import google.cloud.logging
+import logging
+from io import BytesIO
+
+client = google.cloud.logging.Client(project="project")
+client.setup_logging()
+
+def hello_pubsub(cloud_event,context):
+    # Print out the data from Pub/Sub, to prove that it worked
+    print("test-started")
+    logging.info(f"Successfully worked")
