51e227ca18854e5dacd31aab092ec866fe7a5134
warlord-2227 <setu.iitb@gmail.com>
2024-05-13 13:08:54 +0530

    try removing bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,12 @@ data "google_storage_bucket_object" "function_code" {
   name   = "prod/function.zip"
   bucket = data.google_storage_bucket.bucket.name
 }
+resource "google_storage_bucket" "bucket" {
+  name     = "hello-pubsub-function-bucket"
+  location = var.region
+  # Setting force_destroy to false to prevent accidental bucket deletion
+  force_destroy = false
+}
 
 resource "google_cloudfunctions_function" "default" {
   name                  = var.function_name
