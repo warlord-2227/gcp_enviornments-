38e0429d048ef81a6575cad374bdf24848179b8f
warlord-2227 <setu.iitb@gmail.com>
2024-05-13 14:54:40 +0530

    changed workspace

--- src/functions/main.py
+++ src/functions/main.py
@@ -15,5 +15,5 @@ client.setup_logging()
 
 def hello_pubsub(cloud_event,context):
     # Print out the data from Pub/Sub, to prove that it worked
-    print("prod-started now")
+    print("testing-started now")
     logging.info(f"Successfully worked")
